2417c6e7d6ff0739d957a47f375a45905297dd51
Poorna Premachandra <17600522+poorna-premachandra@users.noreply.github.com>
2024-02-11 13:56:13 +0530

    Code refactor

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. I am changed
         </p>
+        <p>I'm deployed through AWS Elastic Beanstalk!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
